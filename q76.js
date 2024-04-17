533fa57fcc5a6a7072f418e2c0f6d9b49b751e2a
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-18 00:24:28 +0500

    File of q76

---
+++ q76.js
@@ -0,0 +1,5 @@
+//Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together, and returns the result.
+function addNumbers(num1, num2) {
+    return num1 + num2; // Calculates the sum of num1 and num2
+}
+console.log(addNumbers(6, 8)); // Outputs 14
---
+++ q76.ts
@@ -0,0 +1,11 @@
+//Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together, and returns the result.
+
+function addNumbers(num1: number, num2: number): number {
+
+    return num1 + num2;        // Calculates the sum of num1 and num2
+}
+
+console.log(addNumbers (6, 8));       // Outputs 14
+
+
+
